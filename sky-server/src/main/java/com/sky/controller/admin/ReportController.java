7586d5d533099806d3c37a2a9ef47cb5d3ee1bf1
kunlunzang <lerbonjames18@gmail.com>
2024-08-13 22:09:55 +1000

    销量排名统计

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -1,8 +1,12 @@
 package com.sky.controller.admin;
 
+import com.aliyun.oss.common.utils.StringUtils;
+import com.sky.dto.GoodsSalesDTO;
+import com.sky.mapper.OrderMapper;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 /*
 数据统计相关的接口
@@ -27,6 +33,8 @@ import java.time.LocalDateTime;
 public class ReportController {
     @Autowired
     private ReportService reportService;
+    @Autowired
+    private OrderMapper orderMapper;
     /**
      * 营业额统计
      * @param begin
@@ -70,6 +78,26 @@ public class ReportController {
 
         return Result.success(reportService.getOrderStatistics(begin,end));
     }
+
+
+    /**
+     * 销量排名统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/top10")
+    @ApiOperation("销量排名统计")
+    public Result<SalesTop10ReportVO> top10(
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
+        return Result.success(reportService.getSalesTop10(begin,end));
+    }
+
+
+
+
+
 
 
 
